c5f1039b285c65a1104f0b9c0522db499f952148
yaotutu <957810668@qq.com>
2024-10-11 11:40:30 +0800

    Add project dependencies and configuration files

---
+++ demo/which-react.ts
@@ -0,0 +1,32 @@
+// import React, {
+//   Component,
+//   PureComponent,
+//   useReducer,
+//   useState,
+//   useEffect,
+//   useLayoutEffect,
+//   useMemo,
+//   useCallback,
+//   useRef,
+//   createContext,
+//   useContext,
+// } from "react";
+// import ReactDOM from "react-dom/client";
+
+// export {
+//   ReactDOM,
+//   Component,
+//   PureComponent,
+//   useReducer,
+//   useState,
+//   useEffect,
+//   useLayoutEffect,
+//   useMemo,
+//   useCallback,
+//   useRef,
+//   createContext,
+//   useContext,
+// };
+
+// export * from "../packages/react/src";
+// export * as ReactDOM from "../packages/react-dom/src/client";
